224504dc32eaaffc02576528b3fe99b3978b6aaf
FreddieTree <freddie0428@outlook.com>
2024-05-21 00:41:04 +0100

    Update .env

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,6 +7,8 @@ import ProjectDetails from '../ProjectDetails/ProjectDetails';
 import CreateProject from '../CreateProject/CreateProject';
 import './Home.css';
 
+let requestInProgress = false;
+
 function Home() {
   const [projects, setProjects] = useState([]);
   const [filteredProjects, setFilteredProjects] = useState([]);
@@ -16,8 +18,12 @@ function Home() {
 
   const fetchProjects = async () => {
     try {
-      const response = await axios.get('/api/projects');
-      setProjects(response.data);
+      const response = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/projects`);
+      if (Array.isArray(response.data)) {
+        setProjects(response.data);
+      } else {
+        console.error('Expected an array but received:', response.data);
+      }
     } catch (error) {
       console.error('Error fetching projects:', error);
     }
@@ -25,18 +31,21 @@ function Home() {
 
   const getUserAddress = async () => {
     try {
-      const existingRequests = await window.ethereum.request({ method: 'wallet_getPermissions' });
-      if (existingRequests.length > 0) {
+      if (requestInProgress) {
         console.log('There is already a pending request');
         return;
       }
+      requestInProgress = true;
 
       await window.ethereum.request({ method: 'eth_requestAccounts' });
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const signer = provider.getSigner();
       const address = await signer.getAddress();
       setUserAddress(address.toLowerCase());
+
+      requestInProgress = false;
     } catch (error) {
+      requestInProgress = false;
       console.error('Error getting user address:', error);
     }
   };
