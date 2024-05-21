5eb41c7c1da2719e34cca46afd60492ac2a86a02
FreddieTree <freddie0428@outlook.com>
2024-05-21 15:43:39 +0100

    analysis

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CreateProject from './components/CreateProject/CreateProject';
 import Home from './components/Home/Home';
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <SpeedInsights />
+        <Analytics />
         <Routes>
           <Route path="/create-project" element={<CreateProject />} />
           <Route path="/" element={<Home />} />
